5df8b5d94e7fb1a82a80b1aadc9dac4b19507cf2
Adrien Zafiropoulos <adrien.zafiropoulos@gmail.com>
2018-05-15 09:31:37 +0200

    adding gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,10 @@ class Article < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, uniqueness: true
-  validates :chapo, lenght: { minimun: 10 }
-  validates :content, lenght: { minimun: 20}
+  validates :chapo, length: { minimum: 10 }
+  validates :content, length: { minimum: 20}
+
+  #photo with cloudinary and carrierwaves
+  mount_uploader :photo, PhotoUploader
+
 end
